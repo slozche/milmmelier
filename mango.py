60436728d616a47626dc768ebee52215d640577a
jonghyeon <102267207+pyeonggyongjang@users.noreply.github.com>
2022-06-23 01:04:11 +0900

    Update mango.py

--- mango.py
+++ mango.py
@@ -2,26 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://port99:BBRnaAd7f3Hadhrq@cluster0.5l5eay2.mongodb.net/?retryWrites=true&w=majority')
-db = client.milmmelier
-
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('http://www.mangoplate.com/search/%EB%B0%80%ED%81%AC%ED%8B%B0', headers=headers)
-
-soup = BeautifulSoup(data.text, 'html.parser')
-cafes = soup.select('body > main > article > div.column-wrapper > div > div > section > div.search-list-restaurants-inner-wrap > ul > li')
-for cafe in cafes:
-    a = cafe.select_one('figure > figcaption > div > a > h2')
-    if a is not None:
-        title = " ".join(a.text.split())
-        star = cafe.select_one('div:nth-child(1) > figure > figcaption > div > strong').text
-        address = cafe.select_one('div:nth-child(1) > figure > a > div > img')['alt'].split("-",maxsplit=1)[1]
-
-        headers = {import requests
-from bs4 import BeautifulSoup
-
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://port99:BBRnaAd7f3Hadhrq@cluster0.5l5eay2.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://jong6598:hyun4198@cluster0.tuvzlln.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.milmmelier
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
